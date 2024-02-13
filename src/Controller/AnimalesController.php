3cdb9a23d865c8c388c492822eef8865fe05c764
Ocho93 <ar.sanchez.p93@gmail.com>
2024-02-13 19:42:57 +0100

    cambio ruta

--- src/Controller/AnimalesController.php
+++ src/Controller/AnimalesController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/animales')]
+#[Route('/')]
 class AnimalesController extends AbstractController
 {
     #[Route('/', name: 'app_animales_index', methods: ['GET'])]
