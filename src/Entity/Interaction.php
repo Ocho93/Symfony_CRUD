b34355d6960d550ca3162eef7e56eef839ebf378
Ocho93 <ar.sanchez.p93@gmail.com>
2024-02-15 13:31:22 +0100

    entity

---
+++ src/Entity/Interaction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InteractionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InteractionRepository::class)]
+class Interaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $User_favorite = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $comment = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isUserFavorite(): ?bool
+    {
+        return $this->User_favorite;
+    }
+
+    public function setUserFavorite(?bool $User_favorite): static
+    {
+        $this->User_favorite = $User_favorite;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
